0785c6ac0c54e035c63ed1bbcc2d26dc99d06b9c
Станислав <@->
2017-09-14 13:35:25 +0300

    1

--- ЕXCEPTION-Lesson13/Program.cs
+++ ЕXCEPTION-Lesson13/Program.cs
@@ -10,6 +10,81 @@ namespace ЕXCEPTION_Lesson13
     {
         static void Main(string[] args)
         {
+
+            bool b = true;
+            int num = 0;
+
+            do
+            {
+                try
+                {
+                    num = numInput();
+
+                    Console.WriteLine("Random Number is " + generate(num));
+                    b = false;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Wrong input. " + fe.Message);
+                }
+                catch (ArithmeticException ae)
+                {
+                    Console.WriteLine("Wrong input. " + ae.Message);
+                }
+            } while (b);
+
+            /////////////////////////////////////////////////////////
+            //bool flag = true;
+            //int num1, num2;
+            //double div ;
+
+            //do
+            //{
+            //    try
+            //    {
+            //        num1 = numInput();
+            //        num2 = numInput();
+
+            //        div = num1 / num2;
+            //        Console.WriteLine("{0} / {1} = {2}", num1, num2, div);
+            //        flag = false;
+
+            //    }
+            //    catch (DivideByZeroException dz)
+            //    {
+            //        Console.WriteLine(dz.Message);
+            //    }
+            //} while (flag);
+            /////////////////////////////////////////////////////////////
+
+
+
+
+            Console.ReadLine();
         }
+
+        static int generate(int num)
+        {
+            int result;
+            if (num < 0 || num > 255)
+                {
+                    throw new ArithmeticException();
+                } else
+                {
+                    Random rand = new Random();
+                    result = rand.Next(num);
+                }
+            return result;
+        }
+
+        static int numInput()
+        {
+            int num;
+            Console.Write("Enter number : ");
+            num = int.Parse(Console.ReadLine());
+
+            return num;
+        }
+        
     }
 }
